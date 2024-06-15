e4c31d3a0e2d7818bf4be7a2f168e6b4cc6e4741
Andrew Makarov <me@r3nya.ru>
2024-06-15 22:30:36 +0200

    Add more info

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
       defaultLocale: "ru",
       locales: {
         ru: {
-          label: 'Русский',
+          label: "Русский",
         },
       },
       lastUpdated: true,
