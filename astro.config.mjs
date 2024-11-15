5622fd6cbc7b8c95a79d1bd14e2e4e343d39d7af
Andrew M. <me@r3nya.ru>
2024-11-15 23:54:59 +0100

    Add `/services` slot (#105)
    * Add structure for `/services`
    
    * add pages
    
    * Restore

--- astro.config.mjs
+++ astro.config.mjs
@@ -130,8 +130,12 @@ export default defineConfig({
           autogenerate: { directory: "housing" },
         },
         { label: "🫒 Еда", autogenerate: { directory: "food" } },
-        { label: "🇪🇸 Испанский язык", autogenerate: { directory: "spanish" } },
+        {
+          label: "🧞 Услуги и сервисы",
+          autogenerate: { directory: "services" },
+        },
         { label: "🚗 Транспорт", autogenerate: { directory: "transport" } },
+        { label: "🇪🇸 Испанский язык", autogenerate: { directory: "spanish" } },
       ],
     }),
     sitemap(),
