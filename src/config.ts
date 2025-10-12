dce457a1baaef7f329337fd3c9af9a0e196201d6
Andrew M. <me@r3nya.ru>
2025-10-12 14:12:42 +0200

    Add BMC button (#248)

--- src/config.ts
+++
@@ -1,4 +0,0 @@
-export const SITE_CONFIG = {
-  email: 'hola-madrid101@pm.me',
-  github: 'https://github.com/r3nya/madrid101',
-} as const;
---
+++ src/data/config.ts
@@ -0,0 +1,5 @@
+export const SITE_CONFIG = {
+  email: "hola-madrid101@pm.me",
+  github: "https://github.com/r3nya/madrid101",
+  bmc: "https://buymeacoffee.com/madrid101",
+};
