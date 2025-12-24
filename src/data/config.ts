6990752acc59716f64a12d3df50098d0b77c48a3
Andrew Makarov <me@r3nya.ru>
2025-12-24 22:05:56 +0100

    Add snow fall effect

--- src/data/config.ts
+++ src/data/config.ts
@@ -2,4 +2,5 @@ export const SITE_CONFIG = {
   email: "hola-madrid101@pm.me",
   github: "https://github.com/r3nya/madrid101",
   bmc: "https://buymeacoffee.com/madrid101",
+  enableSnowFall: true,
 };
